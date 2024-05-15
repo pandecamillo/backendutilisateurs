f48549abe3ec9ee1d37ea0618381f8febbba3943
pandecamillo <160549468+pandecamillo@users.noreply.github.com>
2024-05-15 07:37:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-import express from "express"
+import express, { json } from "express";
 import mysql from "mysql2"
 import cors from "cors"
-import bodyParser from "body-parser"
 
 const port = process.env.port || 8800;
 
@@ -18,17 +17,15 @@ db.getConnection(()=>{
     console.log("connected to database !")
 })
 
-app.use(cors())
-app.use(bodyParser.json({limit: '50mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:100000}));
-app.use(bodyParser.text({ limit: '200mb' }));
+app.use(cors());
+app.use(json());
 
 app.get("/",(req,res)=>{
   res.json("hello this is the backend")
 })
 
 app.get("/users",(req,res)=>{
-    const q = "SELECT  `usernom`, `userprenom` FROM user"
+    const q = "SELECT  `usernom`, `userprenom`, `userphone` FROM user"
     db.query(q, (err,data)=>{
         if(err)return res.json(err)
         console.log("read all users")
@@ -38,7 +35,7 @@ app.get("/users",(req,res)=>{
 
 
 app.post("/user", async(req,res)=>{
-    const q = "INSERT INTO user (`userprofile`, `usernom`, `userprenom`, `userphone`) VALUES (?,?,?,?)"
+    const q = "INSERT INTO user (`usernom`, `userprenom`, `userphone`) VALUES (?,?,?,?)"
     const values = [
         req.body.userprofile,
         req.body.usernom,
@@ -55,7 +52,7 @@ app.post("/user", async(req,res)=>{
 
 app.put("/user/:id", async (req,res)=>{
     const userid = req.params.id;
-    const q = "UPDATE user SET usernom = ?, userprenom = ?, userprofile = ?, userphone = ? WHERE userid = ?"
+    const q = "UPDATE user SET usernom = ?, userprenom = ?, userphone = ? WHERE userid = ?"
     const values = [
         req.body.usernom,
         req.body.userprenom,
@@ -81,7 +78,7 @@ app.delete("/user/:id", async (req,res)=>{
 
 app.get("/user/:id",(req,res)=>{
     const userid = req.params.id;
-    const q = "SELECT * FROM user WHERE userid = ?"
+    const q = "SELECT  `usernom`, `userprenom`, `userphone` FROM user WHERE userid = ?"
     db.query(q, [userid], (err,data)=>{
     if(err)return res.json(err)
         return res.json(data)
